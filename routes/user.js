1989183ef6a3ec4024aca5d58eddffc72c57d106
Raul Rodriguez <raul.i.rodriguez@gmail.com>
2021-03-13 18:37:38 -0600

    Updated create path to register

--- routes/user.js
+++ routes/user.js
@@ -8,10 +8,10 @@ import userCtrl from '../controllers/users.js';
 // User Routes
 router.get('/', userCtrl.index );
 
-router.post('/create', userCtrl.createUser );
-router.get('/:userId',  userCtrl.showUser );
-router.put('/:userId/update', userCtrl.updateUser );
-router.delete('/:userId/delete', userCtrl.destroy );
+router.post('/register', userCtrl.registerUser );
+// router.get('/:userId',  userCtrl.showUser );
+// router.put('/:userId/update', userCtrl.updateUser );
+// router.delete('/:userId/delete', userCtrl.destroy );
 
 
 export { router as user };
