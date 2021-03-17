b8d6eb0b818c0087bd672208d4a85696cfb874ad
GH <gh@GHs-MacBook-Pro.local>
2021-03-16 19:48:44 -0500

    added unique to email and username and removed required on topArtists

--- models/User.js
+++ models/User.js
@@ -5,10 +5,12 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+      unique: true,
     },
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
@@ -24,7 +26,7 @@ const userSchema = new mongoose.Schema(
     },
     userTopArtists: {
         type: String,
-        required: true,
+        required: false,
     }
   },
   {
