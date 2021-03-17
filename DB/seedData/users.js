cb9652c84b8ba18e249e7e2730beda50a290000b
ifrah1 <arifrah125@gmail.com>
2021-03-16 21:46:23 -0400

    unit test to make sure new models work

--- DB/seedData/users.js
+++ DB/seedData/users.js
@@ -2,11 +2,12 @@ import encrypt from '../../auth/encrypt.js';
 
 const usersData = [
     {
-        firstName: "if",
-        lastName: "rah",
-        username: "Arif",
+        username: "arif",
         email: "arif@arif.com",
         password: encrypt.hashPassword('1234'),
+        firstName: "if",
+        lastName: "rah",
+        userTopArtists: ['greg', 'coldplay', 'raul']
     },
 
 ]
