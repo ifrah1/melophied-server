f32bd506cbe472d0f2867c49c02989edf1d43ec5
ifrah1 <arifrah125@gmail.com>
2021-03-19 19:01:37 -0400

    added some seed data for fangpages

---
+++ DB/seedData/fanpage.js
@@ -0,0 +1,15 @@
+const fangPageData = [
+    {
+        author: "605242933784fd0bb0495709",
+        artist: "Arif",
+        pageTitle: 'Test fanPage',
+        pageBio: "if",
+        userTracks: ['greg', 'coldplay', 'raul'],
+        userAlbums: ['greg', 'coldplay', 'raul'],
+        userShows: ['greg', 'coldplay', 'raul'],
+        upvote: ['605242933784fd0bb0495709'],
+    },
+
+]
+
+export default fangPageData;
